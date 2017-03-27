3eb2eb06cd875d84ffe146ea9bb4a6a941a37716
Benjamin Owens <bsowens@bu.edu>
2017-03-27 18:23:11 -0400

    more work on mongo

--- ovalToMongo.py
+++ ovalToMongo.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
-import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
 import os.path
-import pandas as pd
 from bs4 import BeautifulSoup
 import numpy as np
 from pprint import pprint
+import datetime
+import uuid
+
 
 def is_valid_file(parser, arg):
     if not os.path.exists(arg):
@@ -31,6 +32,8 @@ parser.add_argument("-u", dest="user", required=False,
 parser.add_argument("-db", dest="db", required=False,
                     help="Name of database (optional)")
 
+parser.add_argument("-type", dest="type", required=False,
+                    help="Type of scan (xccdf or oval) (optional)")
 args = parser.parse_args()
 
 
@@ -43,7 +46,12 @@ html_string = open(filePath, 'r').read()
 
 
 cl = MongoClient()
-coll = cl["ovalScans"][user]
+# CL
+coll = cl["scans"][user]
+# CL <-| users <-| user
+
+# CL <-| users <-| user
+
 
 soup = BeautifulSoup(html_string, 'lxml')
 topTable = soup.find_all('table', { "border" : "1" })[3]
@@ -53,116 +61,97 @@ resultTable = topTable.find_next_siblings('table')[0]
 
 i=0
 #number of lines to search
-lines = 100
+lines = 10
 
+# add a unique identifier for each scan
 
-
+scanid = str(uuid.uuid4())
 
 #find only applicable lines
-for row in resultTable.find_all('tr',{"class":["resultbadA","resultbadB"]}):
-
+for row in resultTable.find_all('tr',{"class":
+        ["resultbadA","resultbadB","resultgoodA",
+         "resultgoodB","errorA","errorB","unknownA",
+         "unknownB","otherA","otherB"]}):
     rawHtml = str(row)
-    #print(rawHtml)
-
-    #print(row.find_all('td'))
     rowArray = row.find_all('td')
-    #print(rowArray)
-
     resID = rowArray[0].getText()
     resultTF = rowArray[1].getText()
     resultClass = rowArray[2].getText()
     referenceID = rowArray[3].getText()
     referenceLink = rowArray[3].find('a')['href']
+    now = datetime.datetime.now()
+    year  =int(now.year)
+    month  =int(now.month)
+    day  =int(now.day)
+    hour  =int(now.hour)
+    minute  =int(now.minute)
+    second  =int(now.second)
 
-    coll.update(
-        {"username": user},
-        {'$set':
-                {"username": user,
-                 "scans":
-                     {
-                         "_id": ObjectId(),
-                         "timestamp": "12:00:00",
-                         "rawHtml": "<html>blah blah</html>",
-                         "results":
-                             {
-                                 "id": resID,
-                                 "result": resultTF,
-                                 "Class": resultClass,
-                                 "Reference ID": referenceID,
-                                 "Reference Link": referenceLink
-                             }
-                     }
-                 }
+    # format datetime
+    
+    coll.insert(
+        {
+            "scanid" : scanid,
+            "timestamp":
+                {
+                    "year": year ,
+                    "month": month ,
+                    "day": day ,
+                    "hour": hour ,
+                    "minute": minute ,
+                    "second": second
+                },
+
+             "rawHtml": rawHtml,
+             "id": resID,
+             "result": resultTF,
+             "Class": resultClass,
+             "Reference ID": referenceID,
+             "Reference Link": referenceLink
+        }
 
 
-    }
     )
 
-
-    #'''
     i += 1
-    print()
     if i>=lines:
         break
-    #'''
+
 
 
 cursor = coll.find({})
-for document in cursor:
-    pprint(document)
+
 
 '''
 Here's the basic schema for data in the db
 
-{
-     "username": "user123",
-     "attributes": "one,two,three",
-     "scans":
-     {
-         "scanid":"id123",
-         "timestamp": "12:00:00",
-         "rawHtml": "<html>blah blah</html>",
-         "results":
-         {
-            "id" : "oval:com.ubuntu.xenial:def:20176952000",
-            "result" : "true",
-            "Class":"vulnerability",
-            "Reference ID": "[CVE-2017-6952]"
-            "Reference Link": "http://efefwguehf2.com/fuef"
-            "Title" : "CVE-2017-6952 on Ubuntu 16.04 LTS (xenial) - medium."
-        },
-        {
-            "id" : "oval:com.ubuntu.xenial:def:20176952000",
-            "result" : "true",
-            "Class":"vulnerability",
-            "Reference ID": "[CVE-2017-6952]",
-            "Reference Link": "http://efefwguehf2.com/fuef",
-            "Title" : "CVE-2017-6952 on Ubuntu 16.04 LTS (xenial) - medium."
-        }
-     },
-     {
-         "scanid":"id123",
-         "timestamp": "12:00:00",
-         "rawHtml": "<html>blah blah</html>,
-         "results":
-         {
-            "id" : "oval:com.ubuntu.xenial:def:20176952000",
-            "result" : "true",
-            "Class":"vulnerability",
-            "Reference ID": "[CVE-2017-6952]"
-            "Reference Link": "http://efefwguehf2.com/fuef"
-            "Title" : "CVE-2017-6952 on Ubuntu 16.04 LTS (xenial) - medium."
-        },
-        {
-            "id" : "oval:com.ubuntu.xenial:def:20176952000",
-            "result" : "true",
-            "Class":"vulnerability",
-            "Reference ID": "[CVE-2017-6952]"
-            "Reference Link": "http://efefwguehf2.com/fuef"
-            "Title" : "CVE-2017-6952 on Ubuntu 16.04 LTS (xenial) - medium."
-        }
-     }
-}
+{ "_id" : ObjectId("58d98c8e0218239a229b35d8"),
+ "Class" : "vulnerability",
+ "rawHtml" : "<tr class=\"resultbadB\">
+    \n<td align=\"center\" class=\"Text\">
+    oval:com.ubuntu.xenial:def:20176831000</td>
+    \n<td align=\"center\" class=\"Text\">
+    true</td>
+    \n<td align=\"center\" class=\"Text\">
+    vulnerability</td>
+    \n<td align=\"center\" class=\"Text\">
+    [<a class=\"Hover\" href=\"https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-6831\" target=\"_blank\">
+    CVE-2017-6831</a>
+    ]</td>
+    \n<td class=\"Text\">
+    CVE-2017-6831 on Ubuntu 16.04 LTS (xenial) - medium.</td>
+    \n</tr>",
+ "Reference Link" : "https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2017-6831",
+ "Reference ID" : "[CVE-2017-6831]",
+ "id" : "oval:com.ubuntu.xenial:def:20176831000",
+ "result" : "true",
+ "timestamp" : { "month" : 3,
+ "hour" : 18,
+ "second" : 2,
+ "day" : 27,
+ "minute" : 5,
+ "year" : 2017 },
+ "scanid" : "e68f226e-b56a-45f7-8b3b-24c75f6146fc" }
 
 
 
